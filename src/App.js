99601907a1a6f04e9c9b55bdc2d6c0d46739f057
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-10-26 15:35:50 -0400

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import "./App.css"
 
-function App() {
+export default function App() {
+  const [remove, setRemove] = useState("remove")
+  const [add, setAdd] = useState("add")
+  const [count, setCount] = useState(0)
+  const [countArray, setCountArray] = useState([])
+
+  function onRemove(){
+    setCount(count-1)
+    // setCountArray(new Array(count).fill("hi"))
+  }
+
+  const onAdd = ()=>{
+    setCount(count+1)
+    // setCountArray(new Array(count).fill("hi"))
+    console.log(countArray)
+  }
+
+  useEffect(() => {
+    setCountArray(new Array(count).fill("hi"))
+  }, [count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onRemove}>{remove}</button>
+      <div>{count}</div> 
+      <button onClick = {onAdd}>{add}</button>  
+      <div className="star">
+      {countArray.map(()=>{
+          return(
+            <div className="star-box">
+              <img className="star-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Gold_Star.svg/1200px-Gold_Star.svg.png" />
+            </div>
+          )
+        })
+      }
+      </div> 
     </div>
-  );
+  )
 }
-
-export default App;
